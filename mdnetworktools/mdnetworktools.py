dadb333fba20a64d1ff3e3806d1262bb7eda6d88
tdodd3 <75078621+tdodd3@users.noreply.github.com>
2021-01-18 13:30:53 -0500

    Update mdnetworktools.py

--- mdnetworktools/mdnetworktools.py
+++ mdnetworktools/mdnetworktools.py
@@ -1,9 +1,20 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 30 13:10:33 2020
+#   This file is part of the mdnetworktools repository.
+#   Copyright (C) 2020 Ivanov Lab,
+#   Georgia State University (USA)
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Lesser General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-@author: tdodd
-"""
 import numpy as np
 import mdtraj as md
 import timeseriestools as tst
