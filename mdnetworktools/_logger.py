d9e39f50eeea8b6342ad147198973ad9f44637f6
tdodd3 <75078621+tdodd3@users.noreply.github.com>
2021-01-22 14:44:45 -0500

    Update _logger.py

--- mdnetworktools/_logger.py
+++ mdnetworktools/_logger.py
@@ -5,15 +5,15 @@ class LOG(object):
 
 	def __init__(self, logName):
 		logging.basicConfig(filename=logName, filemode='a',
-							format='%(message)s', level=logging.INFO)
+					format='%(message)s', level=logging.INFO)
 							
 		self.inputheader = "################ Input Parameters ################"
 		self.footer = "##################################################"
 		self.attrib = "################## Attributes ####################"
 		self.objs = {0: "Dynamic Network...", 1: "Difference Network...",
-					2: "Girvan-Newman...", 3: "SOAN...", 4: "Average Distances...",
-					5: "Cross-Correlations...", 6: "Contacts...", 7: "Network...",
-					8: "Consensus matrix...", 9: "Difference matrix..."}
+				2: "Girvan-Newman...", 3: "SOAN...", 4: "Average Distances...",
+				5: "Cross-Correlations...", 6: "Contacts...", 7: "Network...",
+				8: "Consensus matrix...", 9: "Difference matrix..."}
 		self.prefx = {0: "Initiating ", 1: "Building ", 2: "Computing "}
 		
 	def _startup(self):
